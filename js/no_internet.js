83273819d983c078ea5d34d82046e7a6a8498fdf
Boelensman1 <wigger.boelens@gmail.com>
2014-07-29 20:13:35 +0200

    nieuwe versie

---
+++ js/no_internet.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function() {
+    check_online(1000)
+});
+function check_online(timeout)
+{
+    if (navigator.onLine == true)
+    {
+        var base_url = 'http://pcwin889.win.tue.nl/2id40-ws/20/';
+        $.ajax({url: base_url + 'time', dataType: 'json'}).always(function(result) {
+            if (result.hasOwnProperty('time')) {
+                //er is weer internet!
+                window.location.href = "index.html";
+            }
+        });
+
+    }
+
+    setTimeout(function() {
+        check_online(timeout)
+    }, timeout);
+}
